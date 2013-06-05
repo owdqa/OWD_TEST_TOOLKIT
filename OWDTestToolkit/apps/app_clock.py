b934264c8c8f7c4567f06deec7d3d1ee6816a0bc
roy <roy.collings@sogeti.com>
2013-06-05 19:08:09 +0200

    Move flick() to action chains in app_clock

--- OWDTestToolkit/apps/app_clock.py
+++ OWDTestToolkit/apps/app_clock.py
@@ -263,10 +263,9 @@ class AppClock(GaiaTestCase):
         y_start = p_scroller.size['height'] / 2
         y_end   = y_start * x
 
-        self.marionette.flick(p_scroller, x_pos, y_start, x_pos, y_end, 270)
-#         actions = Actions(self.marionette)
-#         actions.press(p_scroller, x_pos, y_start).move_by_offset(x_pos, y_end).release()
-#         actions.perform()
+        actions = Actions(self.marionette)
+        actions.flick(p_scroller, x_pos, y_start, x_pos, y_end, 270)
+        actions.perform()
 
 
         time.sleep(0.5)
@@ -281,7 +280,9 @@ class AppClock(GaiaTestCase):
         y_start = p_scroller.size['height'] / 2
         y_end   = y_start / x
         
-        self.marionette.flick(p_scroller, x_pos, y_start, x_pos, y_end, 270)
+        actions = Actions(self.marionette)
+        actions.flick(p_scroller, x_pos, y_start, x_pos, y_end, 270)
+        actions.perform()
 
         time.sleep(0.5)
         
