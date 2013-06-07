7829b2a61b506bd20e3231e9a19d46a7bca46af3
roy <roy.collings@sogeti.com>
2013-06-07 13:10:37 +0200

    Removed 'Keyboard' app - it's already covered by gaiatest.

--- OWDTestToolkit/apps/Keyboard/tap_enter.py
+++
@@ -1,9 +0,0 @@
-from OWDTestToolkit.global_imports import *
-	
-class main(GaiaTestCase):
-
-    def tap_enter(self):
-        self._switch_to_keyboard()
-        self._tap(self._enter_key)
-        self.marionette.switch_to_frame()
-
--- OWDTestToolkit/apps/__init__.py
+++ OWDTestToolkit/apps/__init__.py
@@ -13,5 +13,4 @@ from Marketplace import *
 from Messages import *
 from Settings import *
 from Video import *
-from Keyboard import *
 from Phone import *
