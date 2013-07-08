a59fc434d9b076d6ecdbc62cb5aa6bf064399c95
roy <roy.collings@sogeti.com>
2013-07-08 09:15:40 +0200

    Added 'cache' for Jira results, in case there is a problem fetiching them from the server.

--- bin/get_jira_user_story_test_cases.sh
+++ bin/get_jira_user_story_test_cases.sh
@@ -7,8 +7,14 @@ TYPE=${1:?"Syntax: $0 <test type, or user story id>"}
 U=$(egrep "^U" $HOME/.jira_login | awk '{print $2}')
 P=$(egrep "^P" $HOME/.jira_login | awk '{print $2}')
 
+CACHE_BASE=$HOME/tmp/_jira_test_cases
+[ ! -d "$HOME/tmp" ] && mkdir $HOME/tmp
+
+ME=$(basename $0)
+LOGFILE=${LOGFILE:-"/tmp/_$ME.log"}
+
 #
-# Get the jira id's for test cases.
+# Get the jira id's for the user stories.
 #
 . $OWD_TEST_TOOLKIT_CONFIG/jira_user_stories.sh
 
@@ -67,7 +73,7 @@ do
 	#
 	# Go to JIRA and get the ids (requires you to be in the intranet or VPN).
 	#
-	wget -O /tmp/root_jira_issue.html \
+	wget -O /tmp/_jira_issues_tmp.html \
 	     --no-check-certificate       \
 	     --user=$U --password=$P      \
 	     ${USER_STORIES_BASEURL}${ROOTID}?os_authType=basic >/dev/null 2>&1
@@ -77,7 +83,7 @@ do
 	#
 	awk 'BEGIN{
 	    FOUND = ""
-	    while ( getline < "/tmp/root_jira_issue.html" ){
+	    while ( getline < "/tmp/_jira_issues_tmp.html" ){
 	
 	        if ( $0 ~ /dt title="is tested by/ ){ FOUND = "Y" }
 	
@@ -91,5 +97,28 @@ do
 	        }
 	    }
 	}'
-done
+done | tee $CACHE_BASE.$TYPE.tmp
 
+#
+# If we found nothing, try the previous list (if available).
+#
+x=$(wc -l $CACHE_BASE.$TYPE.tmp 2>/dev/null | awk '{print $1}')
+if [ "$x" == "0" ]
+then
+	rm $CACHE_BASE.$TYPE.tmp
+	printf "$0: WARNING - Unable to return Jira test cases for $TYPE, trying previous list ..." >> $LOGFILE
+	
+	if [ -f $CACHE_BASE.$TYPE ]
+	then
+		cat $CACHE_BASE.$TYPE
+        printf "\n$0:           Sucess!\n\n" >> $LOGFILE
+	else
+	    printf "\n$0:           Failed!! Cannot find previous list, sorry!\n\n" >> $LOGFILE
+	    exit 1
+	fi
+else
+    #
+    # We got the list - refresh the previous list with the new one.
+    #
+    mv $CACHE_BASE.$TYPE.tmp $CACHE_BASE.$TYPE
+fi
