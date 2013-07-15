958b59162a4e5a741ab22c786adcc715a7aff674
roy <roy.collings@sogeti.com>
2013-07-15 17:17:11 +0200

    Fixed the last switchToFrame().

--- OWDTestToolkit/utils/general/setTimeToSpecific.py
+++ OWDTestToolkit/utils/general/setTimeToSpecific.py
@@ -7,6 +7,11 @@ class main(GaiaTestCase):
         # Sets the device time to a specific time based on the parameters
         # (hour is in 24 hour format).
         #
+        if p_hour == 0:
+            p_hour = 0;
+        if p_minute == 0:
+            p_minute = 0;
+            
         h = str(p_hour)
         m = str(p_minute)
         
@@ -25,8 +30,8 @@ class main(GaiaTestCase):
         self.data_layer.set_time(_seconds_since_epoch)
         
         #
-        # I can't find a setting to justgetthe device time, so I look at the clock
-        # in the homescreen.
+        # I can't find a setting to just get the device time, so I wait for the clock
+        # in the homescreen to update.
         #
         myFrame = self.currentIframe()
         self.switchToFrame(*DOM.Home.frame_locator)
@@ -41,8 +46,8 @@ class main(GaiaTestCase):
         self.waitForElements( ("xpath", "//*[@id='landing-clock']/span[@class='numbers' and text()='" + h + ":" + m + "']"), 
                                     "Device showing current time is now " + h + ":" + m, False, 150)
 
-        self.waitForElements( ("xpath", "//*[@id='landing-clock']/span[@class='meridiem' and text()='" + ampm + "'])"), 
+        self.waitForElements( ("xpath", "//*[@id='landing-clock']/span[@class='meridiem' and text()='" + ampm + "']"), 
                               "AM/PM setting is correct", False, 5)
 
-
-        self.switchToFrame(myFrame)
+        if myFrame != "":
+            self.switchToFrame("src", myFrame)
