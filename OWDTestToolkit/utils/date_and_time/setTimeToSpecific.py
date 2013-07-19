256ce7c6f1b9857a1a9def0d29cff3a36dddfbaa
roy <roy.collings@sogeti.com>
2013-07-19 15:29:19 +0200

    Removed obsolete code.

--- OWDTestToolkit/utils/date_and_time/setTimeToSpecific.py
+++ OWDTestToolkit/utils/date_and_time/setTimeToSpecific.py
@@ -46,59 +46,14 @@ class main(GaiaTestCase):
         
         self.data_layer.set_time(_seconds_since_epoch * 1000)
         
-        self.waitForDeviceTimeToChange(p_year,
-                                       p_month,
-                                       p_day,
-                                       p_hour,
-                                       p_minute)
+        self.waitForDeviceTimeToBe(p_year,
+                                   p_month,
+                                   p_day,
+                                   p_hour,
+                                   p_minute)
 
 
 
     
     
     
-        
-        if p_hour == 0:
-            p_hour = 0;
-        if p_minute == 0:
-            p_minute = 0;
-            
-        h = str(p_hour)
-        m = str(p_minute)
-        
-        self.logResult("info", "h: " + h + ", m: " + m)
-        
-        _seconds_since_epoch = self.marionette.execute_script("""
-                var today = new Date();
-                var yr = today.getFullYear();
-                var mth = today.getMonth();
-                var day = today.getDate();
-                return new Date(yr, mth, day, %s, %s, 1).getTime();""" % (h, m) )
-
-        self.today = datetime.datetime.fromtimestamp(_seconds_since_epoch / 1000)
-
-        # set the system date to the time
-        self.data_layer.set_time(_seconds_since_epoch * 1000)
-        
-        #
-        # I can't find a setting to just get the device time, so I wait for the clock
-        # in the homescreen to update.
-        #
-        myFrame = self.currentIframe()
-        self.switchToFrame(*DOM.Home.frame_locator)
-        
-        if p_minute < 10:
-            m = m.zfill(2)
-            
-        x = self.switch_24_12(p_hour)
-        h = str(x[0])
-        ampm = x[1]
-            
-        self.waitForElements( ("xpath", "//*[@id='landing-clock']/span[@class='numbers' and text()='" + h + ":" + m + "']"), 
-                                    "Device showing current time is now " + h + ":" + m, False, 180)
-
-        self.waitForElements( ("xpath", "//*[@id='landing-clock']/span[@class='meridiem' and text()='" + ampm + "']"), 
-                              "AM/PM setting is correct", False, 5)
-
-        if myFrame != "":
-            self.switchToFrame("src", myFrame)
