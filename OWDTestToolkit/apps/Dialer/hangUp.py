b4f587430a2e5bc47847983bd23fd710a8a679c8
roy <roy.collings@sogeti.com>
2013-07-24 20:47:12 +0200

    hangup now uses gaiatest kill call method as well (just to be sure).

--- OWDTestToolkit/apps/Dialer/hangUp.py
+++ OWDTestToolkit/apps/Dialer/hangUp.py
@@ -6,5 +6,14 @@ class main(GaiaTestCase):
         #
         # Hangs up (assuming we're in the 'calling' frame).
         #
-        x = self.UTILS.getElement(DOM.Dialer.hangup_bar_locator, "Hangup bar")
-        x.tap()
+        try:
+	        x = self.UTILS.getElement(DOM.Dialer.hangup_bar_locator, "Hangup bar", True, 5, False)
+	        x.tap()
+        except:
+        	self.UTILS.logResult("info", "<b>NOTE:</b> Could not hangup using the hangup bar!")
+        	pass
+
+	 	#
+	 	# Just to be sure!
+	 	#    
+     	self.data_layer.kill_active_call()
