1d1228ecaa9b6a8c69ff37ad4ec129e8e88223e2
roy <roy.collings@sogeti.com>
2013-08-12 17:20:00 +0200

    Changes to dealing with app icons in homescreen.

--- OWDTestToolkit/utils/app/uninstallApp.py
+++ OWDTestToolkit/utils/app/uninstallApp.py
@@ -8,47 +8,68 @@ class main(GaiaTestCase):
         #
         self.logResult("info", "Making sure app <b>%s</b> is uninstalled." % p_appName)
 
-        #
-        # Verify that the app is installed.
-        #
-        if not self.isAppInstalled(p_appName):
-            return False
-        
-        #
-        # Find the app icon.
-        #
-        myApp = self.findAppIcon(p_appName)
-        if not myApp: return
-        
-        #
-        # We found it! Long-press to into edit mode
-        #
-        self.actions.press(myApp).wait(2).release()
-        self.actions.perform()
-    
-        #
-        # Delete it (and refresh the 'myApp' object to include the new button).
-        #
-        # NOTE: This kind of 'element-within-an-element' isn't necessarily
-        #       appropriate for 'verify', so don't.
-        #
-        delete_button = self.getElement( ("xpath", 
-                                          DOM.Home.app_delete_icon_xpath % p_appName), 
-                                        "Delete button", False, 5, True)
-        delete_button.tap()
-            
-        #
-        # Confirm deletion.
-        #
-        delete = self.getElement(DOM.Home.app_confirm_delete, "Confirm app delete button")
-        delete.tap()
 
-        #
-        # Once it's gone, go home and check the icon is no longer there.
-        #
-        time.sleep(2)
-        self.touchHomeButton()
-        self.touchHomeButton()
-        self.TEST(not self.isAppInstalled(p_appName), "App is uninstalled after deletion.")
-        
-        return True
+        myApp = self.findAppIcon(p_appName)
+        if myApp:  
+            self.actions.press(myApp).wait(2).release()
+            self.actions.perform()
+    
+            delete_button = self.getElement( ("xpath", 
+                                              DOM.Home.app_delete_icon_xpath % p_appName), 
+                                            "Delete button", False, 5, True)
+            delete_button.tap()
+    
+            delete = self.getElement(DOM.Home.app_confirm_delete, "Confirm app delete button")
+            delete.tap()
+    
+            time.sleep(2)
+            self.touchHomeButton()
+    
+            self.TEST(not self.isAppInstalled(p_appName), "App is uninstalled after deletion.")
+
+
+# 
+#         #
+#         # Verify that the app is installed.
+#         #
+#         if not self.isAppInstalled(p_appName):
+#             return False
+#         
+#         #
+#         # Find the app icon.
+#         #
+#         myApp = self.findAppIcon(p_appName)
+#         if not myApp: return
+#         
+#         #
+#         # We found it! Long-press to into edit mode
+#         #
+#         self.actions.press(myApp).wait(2).release()
+#         self.actions.perform()
+#     
+#         #
+#         # Delete it (and refresh the 'myApp' object to include the new button).
+#         #
+#         # NOTE: This kind of 'element-within-an-element' isn't necessarily
+#         #       appropriate for 'verify', so don't.
+#         #
+#         delete_button = self.getElement( ("xpath", 
+#                                           DOM.Home.app_delete_icon_xpath % p_appName), 
+#                                         "Delete button", False, 5, True)
+#         delete_button.tap()
+#             
+#         #
+#         # Confirm deletion.
+#         #
+#         delete = self.getElement(DOM.Home.app_confirm_delete, "Confirm app delete button")
+#         delete.tap()
+# 
+#         #
+#         # Once it's gone, go home and check the icon is no longer there.
+#         #
+#         time.sleep(2)
+#         self.touchHomeButton()
+#         self.touchHomeButton()
+#         self.TEST(not self.isAppInstalled(p_appName), "App is uninstalled after deletion.")
+#         
+#         return True
