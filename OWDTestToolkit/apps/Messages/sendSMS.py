561af0f6338b55fcc162c4a6289d1b8548a694c0
roy <roy.collings@sogeti.com>
2013-08-13 08:51:52 +0200

    Added a 'catch' for when we can't send an sms.

--- OWDTestToolkit/apps/Messages/sendSMS.py
+++ OWDTestToolkit/apps/Messages/sendSMS.py
@@ -12,3 +12,15 @@ class main(GaiaTestCase):
         time.sleep(2) # (Give the spinner time to appear.)
         self.UTILS.waitForNotElements(DOM.Messages.message_sending_spinner, "'Sending' icon", True, 120)
 
+		#
+		# Check if we received the 'service unavailable' message.
+		#
+        try:
+			self.wait_for_element_displayed(*DOM.Messages.service_unavailable_msg, timeout=2)
+			x = self.UTILS.screenShotOnErr()
+			self.UTILS.logResult("info", "'Service unavailable' message detected - unable to send sms!", x)
+			return False
+        except:
+			pass
+		
+        return True
