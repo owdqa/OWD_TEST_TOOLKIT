43b8e7c1deefd04a0512fd73de711429aaaf0e48
roy <roy.collings@sogeti.com>
2013-08-16 09:09:09 +0200

    Removed obsolete 'cd'.

--- bin/execute_tests.sh.parts/set_up_parameters.sh
+++ bin/execute_tests.sh.parts/set_up_parameters.sh
@@ -11,13 +11,9 @@ chmod 777 /tmp/tests 2> /dev/null
 export RUN_TIME=$(date "+%H:%M %d/%m/%Y")
 
 #
-# Directory for all output (only required if not coming from ci_test_run.sh).
+# Directory for all output (or fake it if not coming from ci_test_run.sh).
 #
-if [ ! "$RESULT_DIR" ]
-then
-	. $0.parts/fake_ci_server.sh
-	cd $RESULT_DIR
-fi
+[ ! "$RESULT_DIR" ] && . $0.parts/fake_ci_server.sh
 
 
 #
