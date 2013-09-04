da1ae2938565baedfea7244d5d6928be6db9f820
roy <roy.collings@sogeti.com>
2013-09-04 17:41:13 +0200

    Fixed DOM changes in moveWeekViewBy()

--- OWDTestToolkit/apps/Contacts/viewContact.py
+++ OWDTestToolkit/apps/Contacts/viewContact.py
@@ -10,6 +10,13 @@ class main(GaiaTestCase):
         # In some cases you don't want this to check the header (if the contact has no name,
         # or you're just using the given name etc..). In that case, set p_HeaderCheck=False.
         #
+        
+        #
+        # Because this can be called from several applications (contacts, dialer, sms ...), finding
+        # the right iframe can be tricky, which is why "self._findThisFrame()" is here.
+        #
+        self._orig = self.UTILS.currentIframe()
+
         time.sleep(1)
         self.UTILS.checkMarionetteOK()
         
@@ -59,25 +66,40 @@ class main(GaiaTestCase):
         #
         # Private method to cater for the fact that *SOMETIMES* the 
         # frame is different if coming from Dialer.
+        #        
+        self.marionette.switch_to_frame()
+        x = self._tryFrame(DOM.Dialer.frame_locator)
+        if x:
+            x = self._tryFrame(DOM.Dialer.call_log_contact_name_iframe)
+            if x:
+                self.UTILS.logResult("info", "<i>Switched to dialer -> contacts iframe.)</i>")
+                return "Dialer"
+
+        self.marionette.switch_to_frame()
+        x = self._tryFrame(DOM.Contacts.frame_locator)
+        if x:
+            return "Contacts"
+            
+        self.marionette.switch_to_frame()
+        x = self._tryFrame(DOM.Messages.frame_locator)
+        if x:
+            return "Messages"
+
+        self.marionette.switch_to_frame()
+        x = self._tryFrame(["src", self._orig])
+        return "Unknown"
+            
+        
+    def _tryFrame(self, p_dom):
+        #
+        # Private function to return an object for the many types of 'return iframes'.
         #
         try:
-            self.marionette.switch_to_frame()
-            self.marionette.switch_to_frame("xpath", 
-                                            "//iframe[contains(@%s,'%s')]" % \
-                                            (DOM.Dialer.frame_locator[0],
-                                             DOM.Dialer.frame_locator[1]))
-            self.marionette.switch_to_frame("xpath", 
-                                            "//iframe[contains(@%s,'%s')]" % \
-                                            (DOM.Dialer.call_log_contact_name_iframe[0],
-                                             DOM.Dialer.call_log_contact_name_iframe[1]))
-            self.UTILS.logResult("info", "<i>Switched to dialer -> contacts iframe.)</i>")
-            _frame = "Dialer"
+            _iframe = ("xpath", "//iframe[contains(@%s,'%s')]" % (p_dom[0], p_dom[1]))
+            self.wait_for_element_present(*_iframe, timeout=2)
+            x = self.marionette.find_element(*_iframe)
+            self.marionette.switch_to_frame(x)
+            return True
         except:
-            self.UTILS.switchToFrame(*DOM.Contacts.frame_locator)
-            _frame = "Contacts"
-            
-        return _frame
-
-        
-        
+            return False
         
