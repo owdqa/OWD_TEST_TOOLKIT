08496da6d53b3e06533cdf0a06bfc96d1f279675
roy <roy.collings@sogeti.com>
2013-09-09 17:58:17 +0200

    Final fixes for today.

--- OWDTestToolkit/apps/Settings/wifi_list_tapName.py
+++ OWDTestToolkit/apps/Settings/wifi_list_tapName.py
@@ -7,8 +7,7 @@ class main(GaiaTestCase):
 		# Tap the network name in the list.
 		#
 		_wifi_name_element = ("xpath", DOM.Settings.wifi_name_xpath % p_wifi_name)
-		x= self.UTILS.getElement(_wifi_name_element, "Wifi '" + p_wifi_name + "'", True, 30, True)
-		
+		x = self.UTILS.getElement(_wifi_name_element, "Wifi '%s'" % p_wifi_name, True, 30, True)		
 		x.tap()
 
 		time.sleep(2)
