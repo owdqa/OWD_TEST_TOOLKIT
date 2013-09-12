400a4179e9757f01b5a77d6650979e42aea0bccd
roy <roy.collings@sogeti.com>
2013-09-12 12:43:13 +0200

    Removed debug

--- OWDTestToolkit/utils/statusbar/displayStatusBar.py
+++ OWDTestToolkit/utils/statusbar/displayStatusBar.py
@@ -11,5 +11,3 @@ class main(GaiaTestCase):
         #
         self.marionette.switch_to_frame()
         self.marionette.execute_script("window.wrappedJSObject.UtilityTray.show()")
-        
-        self.TEST(False, "ROY")
