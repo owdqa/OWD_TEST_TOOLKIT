c7b465361776bf1d8c23325643d8a0ea0150a91d
roy <roy.collings@sogeti.com>
2013-09-13 17:30:18 +0200

    Added DOM for modal selection lists.

--- OWDTestToolkit/DOM/GLOBAL.py
+++ OWDTestToolkit/DOM/GLOBAL.py
@@ -5,6 +5,7 @@ app_titlebar_name    = ('class name', 'titlebarIcon')
 modal_confirm_ok     = ("id", "modal-dialog-confirm-ok")
 modal_alert_ok       = ("id", "modal-dialog-alert-ok")
 modal_valueSel_ok    = ('css selector', 'button.value-option-confirm')
+modal_valueSel_list  = ("xpath", "//section[@id='value-selector-container']//li")
 # conf_screen_ok_button = ("class name","value-option-confirm affirmative full")
 conf_screen_ok_button = ('xpath', '//button[@class="value-option-confirm affirmative full"]')
 
