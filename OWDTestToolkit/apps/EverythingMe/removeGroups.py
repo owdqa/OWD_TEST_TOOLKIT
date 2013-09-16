67323dd098138fe3eb4ead3be531beb969347d92
roy <roy.collings@sogeti.com>
2013-09-16 19:09:58 +0200

    Finalized last changes

--- OWDTestToolkit/apps/EverythingMe/removeGroups.py
+++ OWDTestToolkit/apps/EverythingMe/removeGroups.py
@@ -2,10 +2,11 @@ from OWDTestToolkit.global_imports import *
     
 class main(GaiaTestCase):
          
-    def removeGroups(self, p_groupArray=False):
+    def removeGroups(self, p_groupArray=False, p_validate=True):
         #
         # Removes groups from the EME group page.<br>
         # <b>p_groupArray</p> is an array of group names (default = all groups).<br>
+        # <b>p_validate</p> check that the groups were removed (default = True).<br>
         # <br>
         # For example: <i> removeGroups(["Games","Local"])
         #
@@ -14,20 +15,44 @@ class main(GaiaTestCase):
         counter = -1
          
         #
-        # Put the groups into edit mode.
+        # Put the groups into edit mode (sometimes takes a while!).
         #
-        x = self.UTILS.getElements(DOM.EME.groups, "Groups")
-        self.actions.press(x[0]).wait(2).release().perform()
+        _boolOK = False
+        for i in range(0,5):
+            self.wait_for_element_present(*DOM.EME.groups, timeout=2)
+            x = self.marionette.find_elements(*DOM.EME.groups)
+            self.actions.press(x[0]).wait(3 + i).release().perform()
+            
+            try:
+	            if x[0].find_element(*DOM.EME.remove_group_icon).is_displayed():
+	                _boolOK = True
+	                break
+            except:
+            	pass
+               
+            time.sleep(2)
+               
+ 
+        self.UTILS.TEST(_boolOK, "Enabled EDIT mode.")
+        x = self.UTILS.screenShotOnErr()
+        self.UTILS.logResult("info", "Screenshot of app in EDIT mode:", x)
  
         #
         # Remove all groups in the array.
         #
-        _removed = 0
-        _index   = 0
-        for i in range(0,len(x)):
-            _index = i - _removed
+        x          = self.marionette.find_elements(*DOM.EME.groups)
+        _removed   = 0
+        _groupCNT  = len(x)
+        
+        if not p_groupArray:
+            self.UTILS.logResult("info", "Removing all %s groups ..." % _groupCNT)
+        else:
+            self.UTILS.logResult("info", "Removing groups: %s" % str(p_groupArray))
+            
+        for i in range(0,_groupCNT):
             _groupList = self.marionette.find_elements(*DOM.EME.groups)
-            _group     = _groupList[_index]
+            _group     = _groupList[i - _removed]
+            _groupName = _group.get_attribute("data-query")
             
             if p_groupArray:
                 if _removed >= len(p_groupArray):
@@ -37,8 +62,8 @@ class main(GaiaTestCase):
                 # Caller wants to remove specific groups.
                 #
                 for _groupSpecified in p_groupArray:
-                    if _group.get_attribute("data-query"):
-                        if _group.get_attribute("data-query").lower() == _groupSpecified.lower():
+                    if _groupName:
+                        if _groupName.lower() == _groupSpecified.lower():
                             #
                             # Remove it.
                             #
@@ -46,10 +71,10 @@ class main(GaiaTestCase):
                                 x = _group.find_element(*DOM.EME.remove_group_icon)
                                 x.tap()
                                 _removed = _removed + 1
+                                self.UTILS.logResult("info", "Removed group '%s' ..." % _groupName)
                             except:
                                 pass
                             
-                            self.UTILS.logResult("info", "Removed group '%s' ..." % _group.get_attribute("data-query"))
                             break
             else:
                 #
@@ -57,26 +82,36 @@ class main(GaiaTestCase):
                 #
                 try:
                     x = _group.find_element(*DOM.EME.remove_group_icon)
+                    self.UTILS.logResult("debug", "x2")
                     x.tap()
+                    self.UTILS.logResult("debug", "x3")
                     _removed = _removed + 1
+                    self.UTILS.logResult("info", "Removed group '%s' ..." % _groupName)
                 except:
                     pass
-                self.UTILS.logResult("info", "Removed group '%s' ..." % _group.get_attribute("data-query"))
 
+
+        #
+        # Turn off edit mode.
+        #
+        self.UTILS.logResult("info", "Disabling edit mode ...")
+        x = self.marionette.find_element(*DOM.EME.search_field)
+        x.tap()
  
         #
-        # Verify that the group has been removed.
+        # Verify that the groups have been removed.
         #
-        x = self.UTILS.getElements(DOM.EME.groups, "Groups")
-        if not p_groupArray:
-            self.UTILS.TEST(len(x) == 1, "All groups were removed.")
-        else:
-            for groupName in p_groupArray:
-                _boolOK = True
-                for i in x:
-                    if i.get_attribute("data-query"):
-                        if i.get_attribute("data-query").lower() == groupName.lower():
-                            _boolOK = False
-                            break
-                
-                self.UTILS.TEST(_boolOK, "'%s' removed from groups." % groupName)
+        if p_validate:
+            x = self.UTILS.getElements(DOM.EME.groups, "Groups (after editing)")
+            if not p_groupArray:
+                self.UTILS.TEST(len(x) == 1, "All groups were removed.")
+            else:
+                for groupName in p_groupArray:
+                    _boolOK = True
+                    for i in x:
+                        if i.get_attribute("data-query"):
+                            if i.get_attribute("data-query").lower() == groupName.lower():
+                                _boolOK = False
+                                break
+                    
+                    self.UTILS.TEST(_boolOK, "'%s' removed from groups." % groupName)
