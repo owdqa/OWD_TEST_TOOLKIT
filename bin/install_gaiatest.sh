f5958c13408c5f2700c1a502f125fb514025c345
roy <roy.collings@sogeti.com>
2013-09-18 11:43:54 +0200

    Correction to gaia changes.

--- bin/install_gaiatest.sh
+++ bin/install_gaiatest.sh
@@ -25,15 +25,16 @@ then
     printf "(need to clone all of 'gaia' - this will take about 10-15 minutes ...)\n\n" | tee -a $LOGFILE
     cd $HOME
 	git clone https://github.com/mozilla-b2g/gaia.git --depth 1 >> $LOGFILE 2>&1
-
-	printf "\n<b>Switching to branch \"$BRANCH\" of gaiatest ...</b>\n\n" | tee -a $LOGFILE
+	
+    printf "\n<b>Switching to branch \"$BRANCH\" of gaiatest ...</b>\n\n" | tee -a $LOGFILE
+	cd $HOME/gaia
 	git checkout $BRANCH  2> >( tee -a $LOGFILE)
 
 	# Temporary hack...
 	$OWD_TEST_TOOLKIT_BIN/tmp_hack.sh
 else
     printf "(refreshing 'gaia' - this will take just a minute or so ...)\n\n" | tee -a $LOGFILE
-    cd $GAIATEST_PATH/..
+    cd $HOME/gaia
     git fetch origin >> $LOGFILE 2>&1
 fi
 
