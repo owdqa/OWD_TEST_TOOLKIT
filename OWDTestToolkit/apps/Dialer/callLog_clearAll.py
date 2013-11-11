055db2d78b26e0aa51dc2746a834d16d79d54914
David Sanchez <david.sanchez@aurigae.com>
2013-11-11 16:13:53 +0100

    Fixing dialer tests

--- OWDTestToolkit/apps/Dialer/callLog_clearAll.py
+++ OWDTestToolkit/apps/Dialer/callLog_clearAll.py
@@ -1,36 +1,44 @@
 from OWDTestToolkit.global_imports import *
-	
+
+
 class main(GaiaTestCase):
 
     def callLog_clearAll(self):
-        #
-        # Wipes all entries from the csll log.
-        #
-		try:
-			self.wait_for_element_displayed(*DOM.Dialer.call_log_filter, timeout=1)
-		except:
-			self.openCallLog()
-			
-		boolLIST = True
-		try:
-			self.wait_for_element_displayed(*DOM.Dialer.call_log_no_calls_msg, timeout=1)
-			boolLIST = False
-		except:
-			pass
-		
-		if boolLIST:
-			#
-			# At the moment, the 'edit' div looks like it's not displayed, so Marionette can't tap it.
-			# For this reason I'm using JS to click() instead.
-			#
-			self.UTILS.logResult("info", "Some numbers are in the call log here - removing them ...")
-			x = self.UTILS.getElement(DOM.Dialer.call_log_edit_btn, "Edit button")
-			x.tap()
-			time.sleep(2)
-			self.wait_for_element_present(*DOM.Dialer.call_log_edit_selAll, timeout=2)
-			self.marionette.execute_script("document.getElementById('%s').click();" % DOM.Dialer.call_log_edit_selAll[1])
-			time.sleep(1)
-			self.wait_for_element_present(*DOM.Dialer.call_log_edit_delete, timeout=2)
-			self.marionette.execute_script("document.getElementById('%s').click();" % DOM.Dialer.call_log_edit_delete[1])
+    #
+    # Wipes all entries from the csll log.
+    #
+        try:
+            self.wait_for_element_displayed(*DOM.Dialer.call_log_filter, timeout=1)
+        except:
+            self.openCallLog()
 
-		self.UTILS.waitForElements(DOM.Dialer.call_log_no_calls_msg, "'No calls ...' message")
+        boolLIST = True
+        try:
+            self.wait_for_element_displayed(*DOM.Dialer.call_log_no_calls_msg, timeout=1)
+            boolLIST = False
+        except:
+            pass
+
+        if boolLIST:
+            #
+            # At the moment, the 'edit' div looks like it's not displayed, so Marionette can't tap it.
+            # For this reason I'm using JS to click() instead.
+            #
+            self.UTILS.logResult("info", "Some numbers are in the call log here - removing them ...")
+            x = self.UTILS.getElement(DOM.Dialer.call_log_edit_btn, "Edit button")
+            x.tap()
+            time.sleep(2)
+            self.wait_for_element_present(*DOM.Dialer.call_log_edit_selAll, timeout=2)
+            self.marionette.execute_script("document.getElementById('%s').click();" % DOM.Dialer.call_log_edit_selAll[1])
+            time.sleep(1)
+            self.wait_for_element_present(*DOM.Dialer.call_log_edit_delete, timeout=2)
+            self.marionette.execute_script("document.getElementById('%s').click();" % DOM.Dialer.call_log_edit_delete[1])
+
+            self.marionette.execute_script("""
+            var getElementByXpath = function (path) {
+                return document.evaluate(path, document, null, 9, null).singleNodeValue;
+            };
+            getElementByXpath("/html/body/form[3]/menu/button[2]").click();
+            """)
+
+        self.UTILS.waitForElements(DOM.Dialer.call_log_no_calls_msg, "'No calls ...' message")
