f0d5c5e6e37b34562bec653e7856fdf1da47089d
David Sanchez <david.sanchez@aurigae.com>
2013-11-12 13:28:42 +0100

    Fixing wifi tests

--- OWDTestToolkit/apps/Settings/disable_hotSpot.py
+++ OWDTestToolkit/apps/Settings/disable_hotSpot.py
@@ -1,42 +1,41 @@
 from OWDTestToolkit.global_imports import *
-	
+
 class main(GaiaTestCase):
 
     def disable_hotSpot(self):
-        #
-        # Enable hotspot (internet sharing) - asusmes Settings app is already open.
-        #
-		self.UTILS.logResult("info", "<u>Disabling hotspot ...</u>")
-		self.hotSpot()
+    #
+    # Enable hotspot (internet sharing) - asusmes Settings app is already open.
+    #
+        self.UTILS.logResult("info", "<u>Disabling hotspot ...</u>")
 
-		#
-		# Is it already enabled?
-		#
-		x = self.UTILS.getElement(DOM.Settings.hotspot_settings, "Hotspot settings")
-		if x.get_attribute("disabled") == "false":
-			self.UTILS.logResult("info", "Hotspot is already disabled.")
-			return True
-		
-		x = self.UTILS.getElement(DOM.Settings.hotspot_switch, "Hotspot switch")
-		x.tap()
-		time.sleep(1)
-		
-		#
-		# Wait for the hotspot to begin.
-		#
-		boolOK1 = False
-		_tries = 10
-		for i in range(0,_tries):
-			x = self.marionette.find_element(*DOM.Settings.hotspot_settings)
-			if x.get_attribute("disabled") == "false":
-				#
-				# It's done.
-				#
-				boolOK1 = True
-				break
-			time.sleep(0.5)
+        #
+        # Is it already disabled?
+        #
+        x = self.UTILS.getElement(DOM.Settings.hotspot_settings, "Hotspot settings")
+        if x.get_attribute("disabled") == "false":
+            self.UTILS.logResult("info", "Hotspot is already disabled.")
+            return True
 
-		boolOK2 = self.UTILS.isIconInStatusBar(DOM.Statusbar.hotspot)
-		
-		self.UTILS.TEST(boolOK1, "Hotspot settings are enabled (because 'hotspot' is not running).")
-		self.UTILS.TEST(not boolOK2, "Hotspot icon is not present in the status bar.")
+        x = self.UTILS.getElement(DOM.Settings.hotspot_switch, "Hotspot switch")
+        x.tap()
+        time.sleep(1)
+
+        #
+        # Wait for the hotspot to begin.
+        #
+        boolOK1 = False
+        _tries = 10
+        for i in range(0,_tries):
+            x = self.marionette.find_element(*DOM.Settings.hotspot_settings)
+            if x.get_attribute("disabled") == "false":
+                #
+                # It's done.
+                #
+                boolOK1 = True
+                break
+            time.sleep(0.5)
+
+        boolOK2 = self.UTILS.isIconInStatusBar(DOM.Statusbar.hotspot)
+
+        self.UTILS.TEST(boolOK1, "Hotspot settings are enabled (because 'hotspot' is not running).")
+        self.UTILS.TEST(not boolOK2, "Hotspot icon is not present in the status bar.")
--- OWDTestToolkit/apps/Settings/enable_hotSpot.py
+++ OWDTestToolkit/apps/Settings/enable_hotSpot.py
@@ -1,42 +1,41 @@
 from OWDTestToolkit.global_imports import *
-	
+
 class main(GaiaTestCase):
 
     def enable_hotSpot(self):
-        #
-        # Enable hotspot (internet sharing) - asusmes Settings app is already open.
-        #
-		self.UTILS.logResult("info", "<u>Enabling hotspot ...</u>")
-		self.hotSpot()
+    #
+    # Enable hotspot (internet sharing) - asusmes Settings app is already open.
+    #
+        self.UTILS.logResult("info", "<u>Enabling hotspot ...</u>")
 
-		#
-		# Is it already enabled?
-		#
-		x = self.UTILS.getElement(DOM.Settings.hotspot_settings, "Hotspot settings")
-		if x.get_attribute("disabled") == "true":
-			self.UTILS.logResult("info", "Hotspot is already enabled.")
-			return True
-		
-		x = self.UTILS.getElement(DOM.Settings.hotspot_switch, "Hotspot switch")
-		x.tap()
-		time.sleep(1)
-		
-		#
-		# Wait for the hotspot to begin.
-		#
-		boolOK1 = False
-		_tries = 10
-		for i in range(0,_tries):
-			x = self.marionette.find_element(*DOM.Settings.hotspot_settings)
-			if x.get_attribute("disabled") == "true":
-				#
-				# It's done.
-				#
-				boolOK1 = True
-				break
-			time.sleep(0.5)
+        #
+        # Is it already enabled?
+        #
+        x = self.UTILS.getElement(DOM.Settings.hotspot_settings, "Hotspot settings")
+        if x.get_attribute("disabled") == "true":
+            self.UTILS.logResult("info", "Hotspot is already enabled.")
+            return True
 
-		boolOK2 = self.UTILS.isIconInStatusBar(DOM.Statusbar.hotspot)
-		
-		self.UTILS.TEST(boolOK1, "Hotspot settings are disabled (because 'hotspot' is now running).")
-		self.UTILS.TEST(boolOK2, "Hotspot icon is present in the status bar.")
+        x = self.UTILS.getElement(DOM.Settings.hotspot_switch, "Hotspot switch")
+        x.tap()
+        time.sleep(1)
+
+        #
+        # Wait for the hotspot to begin.
+        #
+        boolOK1 = False
+        _tries = 10
+        for i in range(0,_tries):
+            x = self.marionette.find_element(*DOM.Settings.hotspot_settings)
+            if x.get_attribute("disabled") == "true":
+                #
+                # It's done.
+                #
+                boolOK1 = True
+                break
+            time.sleep(0.5)
+
+        boolOK2 = self.UTILS.isIconInStatusBar(DOM.Statusbar.hotspot)
+
+        self.UTILS.TEST(boolOK1, "Hotspot settings are disabled (because 'hotspot' is now running).")
+        self.UTILS.TEST(boolOK2, "Hotspot icon is present in the status bar.")
