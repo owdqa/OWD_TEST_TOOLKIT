9c0cda86798b2b9124e753e6ce4bb047271b87f5
David Sanchez <david.sanchez@aurigae.com>
2013-11-21 11:15:25 +0100

    Fixing the screenshots in the reports

--- OWDTestToolkit/utils/debug/screenShot.py
+++ OWDTestToolkit/utils/debug/screenShot.py
@@ -9,7 +9,7 @@ class main(GaiaTestCase):
         outFile = os.environ['RESULT_DIR'] + "/" + p_fileSuffix + ".png"
         
         try:
-            screenshot = self.marionette.screenshot()[22:] 
+            screenshot = self.marionette.screenshot()
             with open(outFile, 'w') as f:
                 f.write(base64.decodestring(screenshot))        
             return outFile
