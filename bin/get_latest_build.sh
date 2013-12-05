61dbbf6015dd65623d8a9d764e29f848df4c5d15
Open Web Device <owd@owd-local-flash.(none)>
2013-12-05 14:05:52 +0100

    change SOURCE_DIR to owd.tid.es

--- bin/get_latest_build.sh
+++ bin/get_latest_build.sh
@@ -58,7 +58,8 @@ SOURCE_DIR=https://owd.tid.es/releases/DEVELOP/lastest-version
 #
 # Get the latest date (if using the ci2-owd build).
 #
-x=$(echo $SOURCE_DIR | grep ci2-owd)
+#x=$(echo $SOURCE_DIR | grep ci2-owd)
+x=$(echo $SOURCE_DIR | grep owd.tid.es)
 if [ "$x" ]
 then
 	#
