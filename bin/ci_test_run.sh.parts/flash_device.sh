3d796f889704c88e65d7bbe76e50e391127093f4
JPradoGit <jprado@MacBook-Pro-de-Javier.local>
2014-01-10 09:16:02 +0100

    forcing to get the build file

--- bin/ci_test_run.sh.parts/flash_device.sh
+++ bin/ci_test_run.sh.parts/flash_device.sh
@@ -1,7 +1,10 @@
 #
 # Flash device.
 #
-flash_device.sh $DEVICE eng $BRANCH NODOWNLOAD >$RESULT_DIR/flash_device 2>&1
+
+# To get de build file: (without $4 parameter)
+#flash_device.sh $DEVICE eng $BRANCH NODOWNLOAD >$RESULT_DIR/flash_device 2>&1
+flash_device.sh $DEVICE eng $BRANCH >$RESULT_DIR/flash_device 2>&1
 
 export DEVICE_BUILDNAME=$(egrep "^Unpacking " $RESULT_DIR/flash_device | awk '{print $2}' | sed -e "s/^\(.*\).tgz$/\1/")
 mv $RESULT_DIR/flash_device ${RESULT_DIR}/@Flash_device@${DEVICE_BUILDNAME}
