44436402ee3f24c2155e17f4e988f82751cc3271
David Sanchez <david.sanchez@aurigae.com>
2014-01-23 11:02:27 +0100

    Fixing camera tests

--- OWDTestToolkit/apps/Camera/recordVideo.py
+++ OWDTestToolkit/apps/Camera/recordVideo.py
@@ -1,5 +1,6 @@
 from OWDTestToolkit.global_imports import *
-	
+
+
 class main(GaiaTestCase):
 
     def recordVideo(self, p_length):
@@ -7,7 +8,7 @@ class main(GaiaTestCase):
         # Record a video.
         # p_length is the number of seconds to record for.
         #
-        
+
         #
         # Switch to video.
         #
@@ -18,20 +19,14 @@ class main(GaiaTestCase):
         #
         captureBTN = self.UTILS.getElement(DOM.Camera.capture_button, "Capture button")
         captureBTN.tap()
-        
+
         # Record for 5 seconds
-#         self.wait_for_condition(lambda m: m.find_element(*DOM.Camera.video_timer).text == p_length_str_MMSS)
         time.sleep(p_length)
-            
+
         # Stop recording
         captureBTN.tap()
 
-        self.UTILS.waitForNotElements(DOM.Camera.video_timer, "Video timer", True, 10, False);
+        self.UTILS.waitForNotElements(DOM.Camera.video_timer, "Video timer", True, 10, False)
 
-        self.UTILS.waitForElements(DOM.Camera.thumbnail, 
-                                    "Thumbnail appears after recording video", True, 10, False)
-        
-        # TEST: Thumbnail has not been previewed yet.
-        prev_marker = self.UTILS.getElement(DOM.Camera.thumbnail_preview_marker, "Thumbnail preview marker", False)
-        self.UTILS.TEST((prev_marker.get_attribute("class") == "offscreen"), "Image is not previewed as soon as picture is taken.")
-        
+        self.UTILS.waitForElements(DOM.Camera.thumbnail,
+                                   "Thumbnail appears after recording video", True, 10, False)
