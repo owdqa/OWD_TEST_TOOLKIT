7c507ca60e1b008a3252a8893d4f23290e520079
David Sanchez <david.sanchez@aurigae.com>
2014-01-27 16:52:37 +0100

    Fixing video tests

--- OWDTestToolkit/DOM/Video.py
+++ OWDTestToolkit/DOM/Video.py
@@ -1,9 +1,9 @@
 frame_locator           = ('src', 'video')
 
 thumbnails              = ("xpath", "//li[@class='thumbnail']//div[@class='details']")
-thumb_durations         = ("xpath", "//li[@class='thumbnail']//span[@class='after']")
+thumb_durations         = ("xpath", "/html/body/ul/li/ul/li/div/div[3]/span[2]")
 video_frame             = ('id', 'player')
 video_loaded            = ('css selector', 'video[style]')
 current_video_duration  = ("xpath", "//*[@id='duration-text']")
 
-done_button             = ("xpath", "//*[@id='picker-done']")
+done_button             = ("xpath", "//*[@id='picker-done']")26
