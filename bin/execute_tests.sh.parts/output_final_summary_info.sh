ee6955cfaa7d7e0fa588a31178108255b0e41f19
JPradoGit <jprado@MacBook-Pro-de-Javier.local>
2014-01-29 19:36:40 +0100

    set summary results in general file

--- bin/execute_tests.sh.parts/output_final_summary_info.sh
+++ bin/execute_tests.sh.parts/output_final_summary_info.sh
@@ -24,3 +24,35 @@ printf "Ignored test cases                 : %4s\n" $IGNORED
 printf "Unwritten test cases               : %4s\n" $UNWRITTEN
 printf "\n$sep\n\n\n"
 
+
+# Summary to general file...
+
+
+TOTAL_SUM_FILE="/var/www/html/owd_tests/total_sum_file"
+
+if [ "$ON_CI_SERVER" ] && [ ! "$FAKE_CI_SERVER" ]
+then
+    if [ ! -f "$TOTAL_SUM_FILE" ]
+    then
+        # print the header
+        printf "BUILD BEING TESTED\tPossible regression failures\tRUN DETAILS\tStart time\tEnd time\tAutomation failures\tTest cases passed\tAssertions passed\tExpected failures\tIgnored test cases\tUnwritten test cases" | sudo tee $TOTAL_SUM_FILE
+        sudo chmod 755 $TOTAL_SUM_FILE
+    fi
+
+    # print results in one line (each item is separated by tab char)
+    printf "\n" | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s\t" $DEVICE_BUILDNAME | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s\t" $UNEX_FAILS | sudo tee -a $TOTAL_SUM_FILE
+    printf "file://%s/index.html\t" "$HTML_FILEDIR" | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s\t" "$RUN_TIME" | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s\t" "$END_TIME" | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s\n" $AUTOMATION_FAILS | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s / %-4s\n" $P $T | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s / %-4s\n" $ASSERTS_PASSED $ASSERTS_TOTAL | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s\n" $EX_FAILS | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s\n" $IGNORED | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s\n" $UNWRITTEN | sudo tee -a $TOTAL_SUM_FILE
+    printf "\n" | sudo tee -a $TOTAL_SUM_FILE
+
+fi
+
