6e3437ea24a62132bf67ee79dec032d39ade2073
JPradoGit <jprado@MacBook-Pro-de-Javier.local>
2014-01-30 09:04:59 +0100

    summary file in CSV format

--- bin/execute_tests.sh.parts/output_final_summary_info.sh
+++ bin/execute_tests.sh.parts/output_final_summary_info.sh
@@ -29,32 +29,31 @@ printf "\n$sep\n\n\n"
 
 # Summary to general file...
 
-
-TOTAL_SUM_FILE="/var/www/html/owd_tests/total_sum_file"
+TOTAL_SUM_FILE="/var/www/html/owd_tests/total_sum_file.csv"
 
 if [ "$ON_CI_SERVER" ] && [ ! "$FAKE_CI_SERVER" ]
 then
     if [ ! -f "$TOTAL_SUM_FILE" ]
     then
         # print the header
-        printf "JOB NAME\tBUILD BEING TESTED\tPossible regression failures\tRUN DETAILS\tStart time\tEnd time\tAutomation failures\tTest cases passed\tAssertions passed\tExpected failures\tIgnored test cases\tUnwritten test cases" | sudo tee $TOTAL_SUM_FILE
+        printf "JOB NAME,BUILD BEING TESTED,Possible regression failures,RUN DETAILS,Start time,End time,Automation failures,Test cases passed,Assertions passed,Expected failures,Ignored test cases,Unwritten test cases" | sudo tee $TOTAL_SUM_FILE
         sudo chmod 755 $TOTAL_SUM_FILE
     fi
 
     # print results in one line (each item is separated by tab char)
     printf "\n" | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s_%s\t" $JOB_NAME $BUILD_NUMBER | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s\t" $DEVICE_BUILDNAME | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s\t" $UNEX_FAILS | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s/index.html\t" "$HTML_FILEDIR" | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s\t" "$RUN_TIME" | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s\t" "$END_TIME" | sudo tee -a $TOTAL_SUM_FILE
-    printf "%4s\n" $AUTOMATION_FAILS | sudo tee -a $TOTAL_SUM_FILE
-    printf "%4s / %-4s\n" $P $T | sudo tee -a $TOTAL_SUM_FILE
-    printf "%4s / %-4s\n" $ASSERTS_PASSED $ASSERTS_TOTAL | sudo tee -a $TOTAL_SUM_FILE
-    printf "%4s\n" $EX_FAILS | sudo tee -a $TOTAL_SUM_FILE
-    printf "%4s\n" $IGNORED | sudo tee -a $TOTAL_SUM_FILE
-    printf "%4s\n" $UNWRITTEN | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s_%s," $JOB_NAME $BUILD_NUMBER | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s," $DEVICE_BUILDNAME | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s," $UNEX_FAILS | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s/," "$HTML_FILEDIR" | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s," "$RUN_TIME" | sudo tee -a $TOTAL_SUM_FILE
+    printf "%s," "$END_TIME" | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s," $AUTOMATION_FAILS | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s / %-4s," $P $T | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s / %-4s," $ASSERTS_PASSED $ASSERTS_TOTAL | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s," $EX_FAILS | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s," $IGNORED | sudo tee -a $TOTAL_SUM_FILE
+    printf "%4s," $UNWRITTEN | sudo tee -a $TOTAL_SUM_FILE
     printf "\n" | sudo tee -a $TOTAL_SUM_FILE
 
 fi
