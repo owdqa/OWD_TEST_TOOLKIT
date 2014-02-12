8ed6d6d4769a9b2afbe130312fc842964cf27619
JPradoGit <jprado@MacBook-Pro-de-Javier.local>
2014-02-12 12:14:08 +0100

    pase

--- bin/execute_tests.sh.parts/output_final_summary_info.sh
+++ bin/execute_tests.sh.parts/output_final_summary_info.sh
@@ -15,79 +15,23 @@ HTML_FILEDIR=$($0.parts/create_run_results_web_page.sh)
 printf "CLICK HERE FOR RUN DETAILS         : %s\n\n" "$HTML_FILEDIR"
 
 printf "Start time                         : %s\n" "$RUN_TIME"
-printf "End time                           : %s\n\n" "$END_TIME"              
+printf "End time                           : %s\n\n" "$END_TIME"
 
 P=$(($EX_PASSES + $UNEX_PASSES))
 F=$(($EX_FAILS + $UNEX_FAILS))
 T=$(($P + $F))
 
-printf "Automation failures                : %4s\n" $AUTOMATION_FAILS 
-printf "Test cases passed                  : %4s / %-4s\n" $P $T 
-printf "Assertions passed                  : %4s / %-4s\n" $ASSERTS_PASSED $ASSERTS_TOTAL   
-printf "Expected failures                  : %4s\n" $EX_FAILS               
-printf "Ignored test cases                 : %4s\n" $IGNORED              
+printf "Automation failures                : %4s\n" $AUTOMATION_FAILS
+printf "Test cases passed                  : %4s / %-4s\n" $P $T
+printf "Assertions passed                  : %4s / %-4s\n" $ASSERTS_PASSED $ASSERTS_TOTAL
+printf "Expected failures                  : %4s\n" $EX_FAILS
+printf "Ignored test cases                 : %4s\n" $IGNORED
 printf "Unwritten test cases               : %4s\n" $UNWRITTEN
 printf "\n$sep\n\n\n"
 
 
-# Summary to general file...
-
-TOTAL_SUM_FILE="/var/www/html/owd_tests/total_sum_file.csv"
-
-if [ "$ON_CI_SERVER" ] && [ ! "$FAKE_CI_SERVER" ]
-then
-    if [ ! -f "$TOTAL_SUM_FILE" ]
-    then
-        # print the header
-        printf "WEEK NUMBER: %d\n" $(date '+%V') | sudo tee $TOTAL_SUM_FILE
-        printf "TEST SUITE,DEVICE,VERSION,BUILD BEING TESTED,FAILURES,LINK to RUN DETAILS,Test cases passed\n" | sudo tee -a $TOTAL_SUM_FILE
-        sudo chmod 755 $TOTAL_SUM_FILE
-    fi
-
-    # print results in one line (comma separated)
-    printf "\n" | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s," $JOB_NAME | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s," $DEVICE | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s," $BRANCH | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s," $DEVICE_BUILDNAME | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s," $UNEX_FAILS | sudo tee -a $TOTAL_SUM_FILE
-    printf "%s/," "$HTML_FILEDIR" | sudo tee -a $TOTAL_SUM_FILE
-    printf "%4s / %-4s" $P $T | sudo tee -a $TOTAL_SUM_FILE
-    printf "\n" | sudo tee -a $TOTAL_SUM_FILE
-
-fi
-
-# Summary to partial file (device & version)...
-
-PARTIAL_SUM_FILE="/var/www/html/owd_tests/$DEVICE/$BRANCH/partial_sum_file.csv"
-
-if [ "$ON_CI_SERVER" ] && [ ! "$FAKE_CI_SERVER" ]
-then
-    if [ ! -f "$PARTIAL_SUM_FILE" ]
-    then
-        # print the header
-        printf "Week num: %d\n" $(date '+%V') | sudo tee $PARTIAL_SUM_FILE
-        printf "Device: %s\n" $DEVICE | sudo tee -a $PARTIAL_SUM_FILE
-        printf "Version: %s\n" $BRANCH | sudo tee -a $PARTIAL_SUM_FILE
-
-        printf "TEST SUITE,BUILD BEING TESTED,FAILURES,LINK to RUN DETAILS,Test cases passed\n" | sudo tee -a $PARTIAL_SUM_FILE
-        sudo chmod 755 $PARTIAL_SUM_FILE
-    fi
-
-    # print results in one line (comma separated)
-    printf "\n" | sudo tee -a $PARTIAL_SUM_FILE
-    printf "%s," $JOB_NAME | sudo tee -a $PARTIAL_SUM_FILE
-    printf "%s," $DEVICE_BUILDNAME | sudo tee -a $PARTIAL_SUM_FILE
-    printf "%s," $UNEX_FAILS | sudo tee -a $PARTIAL_SUM_FILE
-    printf "%s/," "$HTML_FILEDIR" | sudo tee -a $PARTIAL_SUM_FILE
-    printf "%4s / %-4s" $P $T | sudo tee -a $PARTIAL_SUM_FILE
-    printf "\n" | sudo tee -a $PARTIAL_SUM_FILE
-
-fi
-
-
-
-# CSV with all data...
+# WEEKLY CSV with all data
+# (weekly: there must be one jenkins job to delete this file and to be renewed per week)...
 
 TOTAL_CSV_FILE="/var/www/html/owd_tests/total_csv_file.csv"
 
@@ -111,7 +55,8 @@ then
     printf "%s," $DEVICE | sudo tee -a $TOTAL_CSV_FILE
     printf "%s," $BRANCH | sudo tee -a $TOTAL_CSV_FILE
     printf "%s," $DEVICE_BUILDNAME | sudo tee -a $TOTAL_CSV_FILE
-    printf "%s/," "$HTML_FILEDIR" | sudo tee -a $TOTAL_CSV_FILE
+    #printf "%s/," "$HTML_FILEDIR" | sudo tee -a $TOTAL_CSV_FILE
+    printf "%s/," "$HTML_FILEDIR" | sed -e "s/http:\/\/owd-qa-server\/owd_tests//g" | sudo tee -a $PARTIAL_CSV_FILE
     printf "%s/," "$RUN_TIME" | sudo tee -a $TOTAL_CSV_FILE
     printf "%s/," "$END_TIME" | sudo tee -a $TOTAL_CSV_FILE
     printf "%4s / %-4s," $P $T | sudo tee -a $TOTAL_CSV_FILE
@@ -125,9 +70,19 @@ then
     printf "%s%%" $ERROR_RATE | sudo tee -a $TOTAL_CSV_FILE
     printf "\n" | sudo tee -a $TOTAL_CSV_FILE
 
+    # TODO 1:
+    # total_weekly: python que lo genere a partir del total_csv con selección de campos (al final de cada job) order: by date desc
+
+    # TODO 2:
+    # partial_weekly: python que lo genere a partir del total_csv con cada device y version. (al final de cada job): order by date desc
+
+
 fi
 
-# partial (device & version) CSV with all data...
+
+
+# DAILY CSV partial (device & version) CSV with all data...
+# (DAILY: from Jenkins, "partial_csv_file.csv" is replaced for "partial_csv_file_NEW.csv" at the end of complete testing per version.)
 
 PARTIAL_CSV_FILE="/var/www/html/owd_tests/$DEVICE/$BRANCH/partial_csv_file_NEW.csv"
 
@@ -155,7 +110,8 @@ then
     printf "%s," $DEVICE | sudo tee -a $PARTIAL_CSV_FILE
     printf "%s," $BRANCH | sudo tee -a $PARTIAL_CSV_FILE
     printf "%s," $DEVICE_BUILDNAME | sudo tee -a $PARTIAL_CSV_FILE
-    printf "%s/," "$HTML_FILEDIR" | sudo tee -a $PARTIAL_CSV_FILE
+    #printf "%s/," "$HTML_FILEDIR" | sudo tee -a $PARTIAL_CSV_FILE
+    printf "%s/," "$HTML_FILEDIR" | sed -e "s/http:\/\/owd-qa-server\/owd_tests//g" | sudo tee -a $PARTIAL_CSV_FILE
     printf "%s/," "$RUN_TIME" | sudo tee -a $PARTIAL_CSV_FILE
     printf "%s/," "$END_TIME" | sudo tee -a $PARTIAL_CSV_FILE
     printf "%4s / %-4s," $P $T | sudo tee -a $PARTIAL_CSV_FILE
@@ -168,5 +124,8 @@ then
     printf "%s," $UNWRITTEN | sudo tee -a $PARTIAL_CSV_FILE
     printf "%s%%" $ERROR_RATE | sudo tee -a $PARTIAL_CSV_FILE
     printf "\n" | sudo tee -a $PARTIAL_CSV_FILE
+
+    # TODO 3:
+    # partial_daily: python que lo genere a partir del partial_csv con selección de campos y sólo bugs (al final de cada tanda) order: by bugs desc
 
 fi
