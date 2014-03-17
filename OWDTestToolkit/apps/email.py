3aa8a3c847ed4abc910a6beac8b87e4ed8e0f852
villenavg <a.villena7@gmail.com>
2014-03-17 08:30:25 +0100

    Repository clean-up

--- OWDTestToolkit/apps/email.py
+++ OWDTestToolkit/apps/email.py
@@ -389,6 +389,54 @@ class Email(object):
         self.UTILS.waitForElements(('xpath', DOM.GLOBAL.app_head_specific.format('Inbox')), "Inbox")
         time.sleep(2)
 
+    def setupAccountFirstStep(self, p_user, p_email, p_pass):
+        #
+        # Set up a new email account in the email app and login.
+        #
+
+        #
+        # If we've just started out, email will open directly to "New Account").
+        #
+        x = self.UTILS.getElement(DOM.GLOBAL.app_head, "Application header")
+        if x.text.lower() != "new account":
+            #
+            # We have at least one emali account setup,
+            # check to see if we can just switch to ours.
+            #
+            if self.switchAccount(p_email):
+                return
+
+            #
+            # It's not setup already, so prepare to set it up!
+            #
+            x = self.UTILS.getElement(DOM.Email.settings_set_btn, "Settings set button")
+            x.tap()
+
+            x = self.UTILS.getElement(DOM.Email.settings_add_account_btn, "Add account button")
+            x.tap()
+
+        #
+        # (At this point we are now in the 'New account' screen by one path or
+        # another.)
+        #
+        self.UTILS.typeThis(DOM.Email.username  , "Username field", p_user , True, True)
+        self.UTILS.typeThis(DOM.Email.email_addr, "Address field" , p_email, True, True)
+        self.UTILS.typeThis(DOM.Email.password  , "Password field", p_pass , True, True)
+
+        self.parent.lockscreen.unlock()
+        self.marionette.switch_to_frame()
+        self.UTILS.switchToFrame(*DOM.Email.frame_locator)
+
+        #
+        # (doesn't always appear when using hotmail)
+        #
+        try:
+            self.wait_for_element_displayed(*DOM.Email.login_next_btn, timeout=5)
+            btn = self.marionette.find_element(*DOM.Email.login_next_btn)
+            btn.tap()
+        except:
+            pass
+
     def switchAccount(self, address):
         #
         # Add a new account.
