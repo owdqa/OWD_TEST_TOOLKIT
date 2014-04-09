50861802f97d57ca717e58f9635584a14266359e
villenavg <a.villena7@gmail.com>
2014-04-09 13:59:44 +0200

    reply, reply_all, forward_msg and attach methods added

--- OWDTestToolkit/apps/email.py
+++ OWDTestToolkit/apps/email.py
@@ -1,4 +1,8 @@
 from OWDTestToolkit import DOM
+from OWDTestToolkit.apps.video import Video
+from OWDTestToolkit.apps.gallery import Gallery
+from OWDTestToolkit.apps.music import Music
+from OWDTestToolkit.apps.camera import Camera
 import time
 
 
@@ -18,6 +22,113 @@ class Email(object):
         self.app = self.apps.launch(self.__class__.__name__)
         self.UTILS.element.waitForNotElements(DOM.GLOBAL.loading_overlay, self.__class__.__name__ + " app - loading overlay")
         return self.app
+
+    def createEmailImage(self):
+
+        attach = self.UTILS.element.getElement(DOM.Email.compose_attach_btn, "Attach button")
+        attach.tap()
+
+        self.marionette.switch_to_frame()
+
+        gallery = self.UTILS.element.getElement(DOM.Email.attach_gallery_btn, "From gallery")
+        gallery.tap()
+
+        self.UTILS.iframe.switchToFrame(*DOM.Gallery.frame_locator)
+
+    def createEmailCameraImage(self):
+        self.camera = Camera(self)
+        
+        attach = self.UTILS.element.getElement(DOM.Email.compose_attach_btn, "Attach button")
+        attach.tap()
+
+        self.marionette.switch_to_frame()
+
+        camera = self.UTILS.element.getElement(DOM.Email.attach_camera_btn, "From Camera")
+        camera.tap()
+
+        self.UTILS.iframe.switchToFrame(*DOM.Camera.frame_locator)
+
+        #
+        # Take a picture.
+        #
+        self.camera.takeAndSelectPicture()
+
+        self.UTILS.iframe.switchToFrame(*DOM.Email.frame_locator)
+
+    def createEmailMusic(self):
+
+        attach = self.UTILS.element.getElement(DOM.Email.compose_attach_btn, "Attach button")
+        attach.tap()
+
+        self.marionette.switch_to_frame()
+
+        music = self.UTILS.element.getElement(DOM.Email.attach_music_btn, "From music")
+        music.tap()
+
+        self.UTILS.iframe.switchToFrame(*DOM.Music.frame_locator)
+
+    def createEmailVideo(self):
+
+        attach = self.UTILS.element.getElement(DOM.Email.compose_attach_btn, "Attach button")
+        attach.tap()
+
+        self.marionette.switch_to_frame()
+
+        video = self.UTILS.element.getElement(DOM.Email.attach_video_btn, "From video")
+        video.tap()
+
+        self.UTILS.iframe.switchToFrame(*DOM.Video.frame_locator)
+
+    def attach_file(self, attached_type):
+        #
+        # This method is reponsible of attaching a certain file to an email.
+        # NOTE: It does not fill neither the message destinatary nor the message body
+        #
+
+        self.gallery = Gallery(self)
+        self.video = Video(self)
+        self.music = Music(self)
+
+        if attached_type == "image":
+            #
+            # Add an image file
+            #
+            self.UTILS.general.addFileToDevice('./tests/_resources/80x60.jpg', destination='DCIM/100MZLLA')
+
+            self.createEmailImage()
+            self.gallery.clickThumbEmail(0)
+
+        elif attached_type == "cameraImage":
+            #
+            # Add an image file from camera
+            #
+            self.createEmailCameraImage()
+
+        elif attached_type == "video":
+            #
+            # Load an video file into the device.
+            #
+            self.UTILS.general.addFileToDevice('./tests/_resources/mpeg4.mp4', destination='/SD/mus')
+
+            self.createEmailVideo()
+            self.video.clickOnVideoEmail(0)
+        
+
+        elif attached_type == "audio":
+            #
+            # Load an video file into the device.
+            #
+            self.UTILS.general.addFileToDevice('./tests/_resources/AMR.amr', destination='/SD/mus')
+
+            self.createEmailMusic()
+            self.music.click_on_song_email()
+
+
+        else:
+            # self.UTILS.reporting.logResult("info", "incorrect value received")
+            msg = "FAILED: Incorrect parameter received in createAndSendMMS()"\
+                ". attached_type must being image, video or audio."
+            self.UTILS.test.quitTest(msg)
 
     def deleteEmail(self, subject):
         #
@@ -71,7 +182,11 @@ class Email(object):
         #
         # Because this can take a while, try to "wait_for_element..." several times (5 mins).
         #
-        loops = 60
+        MAX_LOOPS = 30
+        loops = MAX_LOOPS
+        self.marionette.execute_script("document.getElementsByClassName('" + \
+                                                       DOM.Email.folder_message_container[1] + \
+                                                       "')[0].scrollIntoView();")
         while loops > 0:
             try:
                 #
@@ -79,6 +194,16 @@ class Email(object):
                 #
                 self.parent.wait_for_element_displayed(*DOM.Email.folder_subject_list, timeout=2)
                 z = self.marionette.find_elements(*DOM.Email.folder_subject_list)
+
+                #
+                # If we've tried several times and found nothing, it is likely that 
+                # we are stuck at some point down the mails list, so let's try
+                # to go to the top
+                #
+                if loops % 10 == 0 and loops != MAX_LOOPS:
+                    self.marionette.execute_script("document.getElementsByClassName('" + \
+                                                       DOM.Email.folder_message_container[1] + \
+                                                       "')[0].scrollIntoView();")
                 pos = 0
                 for i in z:
                     #
@@ -96,7 +221,7 @@ class Email(object):
 
                         return i
 
-                    pos = pos + 1
+                    pos += 1
 
             except:
                 #
@@ -117,7 +242,7 @@ class Email(object):
 
             time.sleep(5)
 
-            loops = loops - 1
+            loops -= 1
 
         return False
 
@@ -179,7 +304,15 @@ class Email(object):
                 self.parent.wait_for_element_displayed(*DOM.Email.open_email_from)
                 ok = True
             except:
-                ok = False
+                #
+                # Try once again, before giving up
+                #
+                self.UTILS.element.simulateClick(myEmail)
+                try:
+                    self.parent.wait_for_element_displayed(*DOM.Email.open_email_from)
+                    ok = True
+                except:
+                    ok = False
 
             return ok
         else:
@@ -269,15 +402,167 @@ class Email(object):
 
         #
         # Put items in the corresponsing fields.
-        #
-        self.UTILS.general.typeThis(DOM.Email.compose_to, "'To' field", p_target, True, False)
+        # 
+        if type(p_target) is list:
+            for addr in p_target:
+                self.UTILS.general.typeThis(DOM.Email.compose_to, "'To' field", addr, True, False, True, False)
+                self.UTILS.general.typeThis(DOM.Email.compose_to, "'To' field", " ", True, False, True, False)
+        else:
+            self.UTILS.general.typeThis(DOM.Email.compose_to, "'To' field", p_target, True, False)
         self.UTILS.general.typeThis(DOM.Email.compose_subject, "'Subject' field", p_subject, True, False)
         self.UTILS.general.typeThis(DOM.Email.compose_msg, "Message field", p_message, True, False, False)
 
         self.sendTheMessage()
 
+    def reply_msg(self, reply_message):
+        #
+        # This method replies to a previously received message
+        # It assumes we already are viewing that message
+        #
+        
+        #
+        # Get who sent us the email
+        #
+        from_field = self.UTILS.element.getElement(DOM.Email.open_email_from, "'From' field").text
+        
+        reply_btn = self.UTILS.element.getElement(DOM.Email.reply_btn, "Reply message button")
+        reply_btn.tap()
+
+        #
+        # Now choose the "Reply" option
+        #
+        
+        reply_opt = self.UTILS.element.getElement(DOM.Email.reply_menu_reply, "'Reply' option button")
+        reply_opt.tap()
+
+        #
+        # Wait for 'compose message' header.
+        #
+        x = self.UTILS.element.getElement(('xpath', DOM.GLOBAL.app_head_specific.format("Compose")),
+                                  "Compose message header")
+        time.sleep(5)
+        # #
+        # # Get the guy we're replying to
+        # #
+        # to_field = self.UTILS.element.getElement(DOM.Email.compose_to_from_contacts_address, "[Reply] 'To' field", False).text
+
+        # #
+        # # Check we're actually replying to the guy who sent us the email
+        # #
+        # self.UTILS.reporting.logResult("info", "'From' field: {}".format(from_field))
+        # self.UTILS.reporting.logResult("info", "'To' field: {}".format(to_field))
+        # self.UTILS.test.TEST(from_field == to_field, "Checking we are replying correctly")
+
+        #
+        # Write some reply content
+        #
+        self.UTILS.general.typeThis(DOM.Email.compose_msg, "Message field", reply_message, True, True, False)
+
+        self.replyTheMessage(from_field.split("@")[0])
+
+    def reply_all(self, reply_message):
+        #
+        # This method replies to all recipients of a previously received message
+        # It assumes we already are viewing that message
+        #
+        
+        #
+        # Get who sent us the email
+        #
+        from_field = self.UTILS.element.getElement(DOM.Email.open_email_from, "'From' field").text
+        
+        reply_btn = self.UTILS.element.getElement(DOM.Email.reply_btn, "Reply message button")
+        reply_btn.tap()
+
+        #
+        # Now choose the "Reply all" option
+        #
+        reply_opt = self.UTILS.element.getElement(DOM.Email.reply_menu_reply_all, "'Reply all' option button")
+        reply_opt.tap()
+
+        #
+        # Wait for 'compose message' header.
+        #
+        x = self.UTILS.element.getElement(('xpath', DOM.GLOBAL.app_head_specific.format("Compose")),
+                                  "Compose message header")
+        time.sleep(5)
+
+        #
+        # Write some reply content
+        #
+        self.UTILS.general.typeThis(DOM.Email.compose_msg, "Message field", reply_message, True, True, False)
+
+        self.replyTheMessage(from_field.split("@")[0])
+
+    def forward_msg(self, p_target, fwd_message, attach=False, attached_type=None):
+        #
+        # This method forward a previously received message to somebody else
+        # It assumes we already are viewing that message
+        #
+        
+        #
+        # Get who sent us the email
+        #
+        from_field = self.UTILS.element.getElement(DOM.Email.open_email_from, "'From' field").text
+        
+        reply_btn = self.UTILS.element.getElement(DOM.Email.reply_btn, "Reply message button")
+        reply_btn.tap()
+
+        #
+        # Now choose the "Reply all" option
+        #
+        fw_opt = self.UTILS.element.getElement(DOM.Email.reply_menu_forward, "'Forward' option button")
+        fw_opt.tap()
+
+        #
+        # Wait for 'compose message' header.
+        #
+        x = self.UTILS.element.getElement(('xpath', DOM.GLOBAL.app_head_specific.format("Compose")),
+                                  "Compose message header")
+        time.sleep(5)
+
+        #
+        # Put items in the corresponsing fields.
+        # 
+        if type(p_target) is list:
+            for addr in p_target:
+                self.UTILS.general.typeThis(DOM.Email.compose_to, "'To' field", addr, True, False, True, False)
+                self.UTILS.general.typeThis(DOM.Email.compose_to, "'To' field", " ", True, False, True, False)
+        else:
+            self.UTILS.general.typeThis(DOM.Email.compose_to, "'To' field", p_target, True, False)
+
+        #
+        # Write some reply content
+        #
+        self.UTILS.general.typeThis(DOM.Email.compose_msg, "Message field", fwd_message, True, True, False)
+
+        if attach:
+            self.attach_file(attached_type)
+
+        self.replyTheMessage(from_field.split("@")[0])
+
+
+    def replyTheMessage(self, sender_name):
+        #
+        # Hits the 'Send' button to reply to the message (handles
+        # waiting for the correct elements etc...).
+        #
+        
+        x = self.UTILS.element.getElement(DOM.Email.compose_send_btn, "Send button")
+        x.tap()
+        self.UTILS.element.waitForElements(DOM.Email.compose_sending_spinner, "Sending email spinner")
+
+        #
+        # Wait for inbox to re-appear (give it a BIG wait time because sometimes
+        # it just needs it).
+        #
+        self.UTILS.element.waitForNotElements(DOM.Email.compose_sending_spinner, "Sending email spinner", True, 60, False)
+
+        x = ('xpath', DOM.GLOBAL.app_head_specific.format(sender_name))
+        self.UTILS.element.waitForElements(x, "Previous received message", True, 120)
+        
     def sendTheMessage(self):
-    #
+        #
         # Hits the 'Send' button to send the message (handles
         # waiting for the correct elements etc...).
         #
