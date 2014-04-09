25cf0239a248b9c89ce06c843c1144c7c48362ea
Massimo Barone <barone@tid.es>
2014-04-09 10:19:19 +0200

    Updated toolkit

--- OWDTestToolkit/DOM/Music.py
+++ OWDTestToolkit/DOM/Music.py
@@ -1,6 +1,8 @@
-frame_locator           = ('src', 'music')
+frame_locator = ('src', 'music')
 
 
-done_button             = ("xpath", "//*[@id='title-done']")
-song1                   = ("xpath", "/html/body/div/div[2]/div/li[2]/span")
-song                    = ("xpath", "/html/body/div/div/div/div/div/div/div")
+done_button = ("xpath", "//*[@id='title-done']")
+song1 = ("xpath", "/html/body/div/div[2]/div/li[2]/span")
+song = ("xpath", "/html/body/div/div/div/div/div/div/div")
+song_title_amr = ("xpath", "//*[@data-l10n-id='unknownTitle']")
+song_title = ("xpath", "//*[@data-l10n-id='music']")
