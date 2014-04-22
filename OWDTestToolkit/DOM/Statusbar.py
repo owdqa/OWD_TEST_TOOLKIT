86516fd5cce7cbc4db7e68558095f6ba5c18c77b
villenavg <a.villena7@gmail.com>
2014-04-22 09:16:06 +0200

    Added references to toaster notification and to normal notifications

--- OWDTestToolkit/DOM/Statusbar.py
+++ OWDTestToolkit/DOM/Statusbar.py
@@ -15,4 +15,10 @@ toggle_airplane = ("id", "quick-settings-airplane-mode")
 
 settings_button = ("id", "quick-settings-full-app")
 
+notification_toaster_detail = ("xpath", "//div[@id='toaster-detail' and contains(text(),'{}')]")
+notification_toaster_title = ("xpath", "//div[@id='toaster-title' and contains(text(),'{}')]")
+notification_statusbar_title = ('xpath', '//div[@id="desktop-notifications-container"]/div[@class="notification"]/div[contains(text(),"{}")]')
+notification_statusbar_detail = ('xpath', '//div[@id="desktop-notifications-container"]/div[@class="notification"]/div[@class="detail" and contains(text(),"{}")]')
+
+
 
