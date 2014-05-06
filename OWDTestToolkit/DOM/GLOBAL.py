854ed23cbcc7bcde742c15bd2ef4b21978383945
villenavg <a.villena7@gmail.com>
2014-05-06 08:27:39 +0200

    Lockscreen network label locator added

--- OWDTestToolkit/DOM/GLOBAL.py
+++ OWDTestToolkit/DOM/GLOBAL.py
@@ -18,3 +18,4 @@ conf_screen_ok_button = ('xpath', '//button[@class="value-option-confirm affirma
 scroller_curr_val    = ("class name", "picker-unit active")
 pin_input = ("css selector", "#dialog-overlay #simpin-dialog .container #pinArea .input-wrapper input")
 pin_ok_button = ("xpath", '//div[@id="dialog-overlay"]//button[@data-l10n-id="ok"]')
+lockscreen_network_locator = ('xpath', '//div[@id="lockscreen-header"]//span[@class="connstate-line"]')
