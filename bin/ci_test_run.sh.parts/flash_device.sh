7931e100d443bae414bfcafdf4b9a6db504ba5ea
JPradoGit <jprado@MacBook-Pro-de-Javier.local>
2014-05-19 10:33:26 +0200

    not flashing device with preinstalled certification buil

--- bin/ci_test_run.sh.parts/flash_device.sh
+++ bin/ci_test_run.sh.parts/flash_device.sh
@@ -3,8 +3,18 @@
 #
 #flash_device.sh $DEVICE eng $BRANCH NODOWNLOAD >$RESULT_DIR/flash_device 2>&1
 # forcing to download the build file
-flash_device.sh $DEVICE eng $BRANCH >$RESULT_DIR/flash_device 2>&1
 
-export DEVICE_BUILDNAME=$(egrep "^Unpacking " $RESULT_DIR/flash_device | awk '{print $2}' | sed -e "s/^\(.*\).tgz$/\1/")
-mv $RESULT_DIR/flash_device ${RESULT_DIR}/@Flash_device@${DEVICE_BUILDNAME}
+printf "\n\nFLASHING DEVICE - DO NOT DISTURB! :)\n"
+if [ "$DEVICE" = "certification" ]
+then
+    printf "\n\n CERTIFICATION BUILD PREINSTALLED. NOT FLASHING DEVICE!)\n"
+    export DEVICE_BUILDNAME="Certification preinstalled build"
+else
+    flash_device.sh $DEVICE eng $BRANCH >$RESULT_DIR/flash_device 2>&1
+
+    export DEVICE_BUILDNAME=$(egrep "^Unpacking " $RESULT_DIR/flash_device | awk '{print $2}' | sed -e "s/^\(.*\).tgz$/\1/")
+    mv $RESULT_DIR/flash_device ${RESULT_DIR}/@Flash_device@${DEVICE_BUILDNAME}
+fi
+
+
 
