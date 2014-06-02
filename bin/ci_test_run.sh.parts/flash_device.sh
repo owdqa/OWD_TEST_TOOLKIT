ff6ea6b59b78921089f86d81cca81a96aa0803b3
JPradoGit <jprado@MacBook-Pro-de-Javier.local>
2014-06-02 17:07:48 +0200

    add device=build_preinstalled

--- bin/ci_test_run.sh.parts/flash_device.sh
+++ bin/ci_test_run.sh.parts/flash_device.sh
@@ -9,11 +9,17 @@ then
     printf "\n\n CERTIFICATION BUILD PREINSTALLED. NOT FLASHING DEVICE!)\n"
     export DEVICE_BUILDNAME="Cert_preinstalled"
 else
-    printf "\n\nFLASHING DEVICE - DO NOT DISTURB! :)\n"
-    flash_device.sh $DEVICE eng $BRANCH >$RESULT_DIR/flash_device 2>&1
+    if [ "$DEVICE" = "build_preinstalled" ]
+    then
+        printf "\n\n CERTIFICATION BUILD PREINSTALLED. NOT FLASHING DEVICE!)\n"
+        export DEVICE_BUILDNAME="Build_preinstalled"
+    else
+        printf "\n\nFLASHING DEVICE - DO NOT DISTURB! :)\n"
+        flash_device.sh $DEVICE eng $BRANCH >$RESULT_DIR/flash_device 2>&1
 
-    export DEVICE_BUILDNAME=$(egrep "^Unpacking " $RESULT_DIR/flash_device | awk '{print $2}' | sed -e "s/^\(.*\).tgz$/\1/")
-    mv $RESULT_DIR/flash_device ${RESULT_DIR}/@Flash_device@${DEVICE_BUILDNAME}
+        export DEVICE_BUILDNAME=$(egrep "^Unpacking " $RESULT_DIR/flash_device | awk '{print $2}' | sed -e "s/^\(.*\).tgz$/\1/")
+        mv $RESULT_DIR/flash_device ${RESULT_DIR}/@Flash_device@${DEVICE_BUILDNAME}
+    fi
 fi
 
 
