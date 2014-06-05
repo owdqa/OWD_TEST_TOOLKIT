86c1d49a1d4d830b85fb4c87b0ff13c2f440b2ea
JPradoGit <jprado@MacBook-Pro-de-Javier.local>
2014-06-05 12:48:49 +0200

    temporary solution for maintain gaia_test.py modified

--- bin/install_gaiatest.sh
+++ bin/install_gaiatest.sh
@@ -36,11 +36,9 @@ then
 else
     printf "(refreshing 'gaia' - this will take just a minute or so ...)\n\n" | tee -a $LOGFILE
     cd $HOME/gaia
-    git stash
-    #git fetch
-    #sudo git checkout $BRANCH  2> >( tee -a $LOGFILE)
-    #sudo git merge origin/$BRANCH
-    sudo git checkout -f $BRANCH  2> >( tee -a $LOGFILE)
+    #sudo git stash
+    sudo git checkout $BRANCH  2> >( tee -a $LOGFILE)
+    #sudo git checkout -f $BRANCH  2> >( tee -a $LOGFILE)
     sudo git pull 2> >( tee -a $LOGFILE)
 fi
 
