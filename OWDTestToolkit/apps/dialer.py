f996be45738a04afa5aed3436a27478e5c5f63ce
villenavg <a.villena7@gmail.com>
2014-06-23 14:00:54 +0200

    Deleted unnecesary code

--- OWDTestToolkit/apps/dialer.py
+++ OWDTestToolkit/apps/dialer.py
@@ -148,31 +148,6 @@ class Dialer(object):
             self.parent.data_layer.delete_all_call_log_entries()
             self.UTILS.element.waitForElements(DOM.Dialer.call_log_no_calls_msg, "'No calls ...' message")
 
-        # if boolLIST:
-        #     #
-        #     # At the moment, the 'edit' div looks like it's not displayed, so Marionette can't tap it.
-        #     # For this reason I'm using JS to click() instead.
-        #     #
-        #     self.UTILS.reporting.logResult("info", "Some numbers are in the call log here - removing them ...")
-        #     x = self.UTILS.element.getElement(DOM.Dialer.call_log_edit_btn, "Edit button")
-        #     x.tap()
-        #     time.sleep(2)
-        #     self.parent.wait_for_element_present(*DOM.Dialer.call_log_edit_selAll, timeout=2)
-        #     self.marionette.execute_script("document.getElementById('{}').click();".\
-        #                                    format(DOM.Dialer.call_log_edit_selAll[1]))
-        #     time.sleep(1)
-        #     self.parent.wait_for_element_present(*DOM.Dialer.call_log_edit_delete, timeout=2)
-        #     self.marionette.execute_script("document.getElementById('{}').click();".\
-        #                                    format(DOM.Dialer.call_log_edit_delete[1]))
-
-        #     self.marionette.execute_script("""
-        #     var getElementByXpath = function (path) {
-        #         return document.evaluate(path, document, null, 9, null).singleNodeValue;
-        #     };
-        #     getElementByXpath("/html/body/form[3]/menu/button[2]").click();
-        #     """)
-
-
     def callLog_clearSome(self, p_entryNumbers):
         #
         # Wipes entries from the call log, using p_entryNumbers as an array of
