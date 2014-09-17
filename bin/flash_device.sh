0545be6439ac28329fc6addee69b8a0f4e339b6e
JPradoGit <jprado@JPradoMac.local>
2014-09-17 12:45:21 +0200

    adjust RAM in Flame devices to 512M

--- bin/flash_device.sh
+++ bin/flash_device.sh
@@ -157,8 +157,8 @@ gcli killapps
 
 if [ "$DEVICE" = "flame-JB" ]
 then
-    printf "\nFLAME device: adjusting RAM to 319Mb\n"
-    sudo $OWD_TEST_TOOLKIT_BIN/adjustRAM.sh 319 
+    printf "\nFLAME device: adjusting RAM to 512Mb\n"
+    sudo $OWD_TEST_TOOLKIT_BIN/adjustRAM.sh 512
 fi
 
 #
