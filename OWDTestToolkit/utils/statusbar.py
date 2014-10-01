f194ae16ac37f5798e341b2c3d4f32bf14ed6f76
Alberto Villena <alberto.villenaguerras@telefonica.com>
2014-10-01 11:21:26 +0200

    Line commented

--- OWDTestToolkit/utils/statusbar.py
+++ OWDTestToolkit/utils/statusbar.py
@@ -16,7 +16,7 @@ class statusbar(object):
         """
         self.marionette.switch_to_frame()
         utility_tray = self.system.open_utility_tray()
-        utility_tray.wait_for_notification_container_displayed()
+        # utility_tray.wait_for_notification_container_displayed()
         # Gaiatestcase wait_for_notification_container_displayed not working properly
         # so we have to manually wait some time in order to make sure the statusbar 
         # is fully shown
