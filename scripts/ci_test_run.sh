731ca5facdb495f32e058c6717782ac830d11292
Francisco J. Cano Sandoval <milacao@gmail.com>
2014-11-26 13:47:12 +0100

    Fix paths

--- scripts/ci_test_run.sh
+++ scripts/ci_test_run.sh
@@ -23,5 +23,5 @@ export TEST_TYPE=${3:-"REGRESSION"}
 #cd $owd_test_cases_DIR
 cd $OWD_TEST_TOOLKIT_DIR/../owd_test_cases
 
-sudo python ../../OWD_TEST_TOOLKIT/scripts/ffox_test_runner.py --testvars=../../OWD_TEST_TOOLKIT/config/gaiatest_testvars.json --address=localhost:2828 tests/$TEST_TYPE
+sudo python ../OWD_TEST_TOOLKIT/scripts/ffox_test_runner.py --testvars=../OWD_TEST_TOOLKIT/config/gaiatest_testvars.json --address=localhost:2828 tests/$TEST_TYPE
 
