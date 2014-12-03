66fb1efdd566083cef9cdf1bc8fe4bfef075be76
Francisco J. Cano Sandoval <milacao@gmail.com>
2014-12-03 09:20:10 +0100

    Add comment to flash_device

--- scripts/ci_test_run.sh.parts/flash_device.sh
+++ scripts/ci_test_run.sh.parts/flash_device.sh
@@ -17,6 +17,7 @@ else
         printf "\n\nFLASHING DEVICE - DO NOT DISTURB! :)\n"
         $OWD_TEST_TOOLKIT_BIN/flash_device.sh $DEVICE eng $BRANCH >$RESULT_DIR/flash_device 2>&1
 
+        printf "Getting device_buildname from $RESULT_DIR/flash_device\n"
         export DEVICE_BUILDNAME=$(egrep "^Unpacking " $RESULT_DIR/flash_device | awk '{print $2}' | sed -e "s/^\(.*\).tgz$/\1/")
         mv $RESULT_DIR/flash_device ${RESULT_DIR}/@Flash_device@${DEVICE_BUILDNAME}
     fi
