c7d787eabd74adbaef281fb898ee6a540444ffd1
Francisco J. Cano Sandoval <milacao@gmail.com>
2015-03-18 13:10:13 +0100

    New scripts for certification tests in jenkins

---
+++ scripts/ci_cert_test_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Prepare the dependencies for running OWD tests on the CI server.
+#
+# It is assumed that the OWD_TEST_TOOLKIT has already been cloned
+# (or this script would not exist to run!).
+#
+export DEVICE=${1:-"flame-KK"}
+export BRANCH=${2:-"v2.1"}
+export TEST_TYPE=${3:-"REGRESSION"}
+
+OWD_TEST_TOOLKIT_DIR=`dirname $PWD`
+
+. set_up_parameters.sh
+
+python install_cert_tests.py
+
+. flash_device.sh $DEVICE eng $BRANCH > $RESULT_DIR/flash_device 2>&1
+
+cd $OWD_TEST_TOOLKIT_DIR/../owd-obcertification
+
+sudo DEVICE=$DEVICE BRANCH=$BRANCH DEVICE_BUILDNAME=$DEVICE_BUILDNAME ON_CI_SERVER=$ON_CI_SERVER RUN_ID=$RUN_ID python $OWD_TEST_TOOLKIT_DIR/scripts/ffox_test_runner.py --testvars=$OWD_TEST_TOOLKIT_DIR/config/gaiatest_testvars.json --address=localhost:2828 owd_test_cases/tests/$TEST_TYPE --log-tbpl=/tmp/tests/tests.log
